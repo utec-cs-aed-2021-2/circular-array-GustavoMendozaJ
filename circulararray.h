cee0253e29d28a88bcbccf9b2b5097a3cefbdf0f
GustavoMendozaJ <gustavo.mendoza@utec.edu.pe>
2021-09-11 22:20:17 -0500

    all functions added

--- circulararray.h
+++ circulararray.h
@@ -10,22 +10,22 @@ private:
     int back, front;
 
 public:
-    CircularArray();              //done
-    CircularArray(int _capacity); //done
-    virtual ~CircularArray();     //done
-    void push_front(T data);      //done
-    void push_back(T data);       //done
-    void insert(T data, int pos);
-    T pop_front();      //done
-    T pop_back();       //done
-    bool is_full();     //done
-    bool is_empty();    //done
-    int size();         //done
-    void clear();       //done
-    T &operator[](int); //done
-    void sort();
-    bool is_sorted();
-    void reverse();
+    CircularArray();                    //done
+    CircularArray(int _capacity);       //done
+    virtual ~CircularArray();           //done
+    void push_front(T data);            //done
+    void push_back(T data);             //done
+    void insert(T data, int pos);       //done
+    T pop_front();                      //done
+    T pop_back();                       //done
+    bool is_full();                     //done
+    bool is_empty();                    //done
+    int size();                         //done
+    void clear();                       //done
+    T &operator[](int);                 //done
+    void sort();                        //done
+    bool is_sorted();                   //done
+    void reverse();                     //done
     string to_string(string sep = " "); //done
 
 private:
@@ -74,7 +74,7 @@ bool CircularArray<T>::is_empty()
 template <class T>
 bool CircularArray<T>::is_full()
 {
-    return front = next(back) && !is_empty();
+    return front == next(back) && !is_empty();
 }
 
 template <class T>
@@ -160,6 +160,85 @@ T &CircularArray<T>::operator[](int pos)
     if (pos >= capacity)
         throw "Out of range";
     return array[(front + pos) % capacity];
+}
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+template <class T>
+bool CircularArray<T>::is_sorted()
+{
+    if (is_empty())
+        return false;
+    else
+    {
+        int first_el = front;
+        int last_el = back;
+        if (first_el == last_el)
+            return true;
+        else
+        {
+            int i = first_el;
+            while (++i != last_el)
+            {
+                if (*(this->array + i) < *(this->array + first_el))
+                {
+                    return false;
+                }
+                ++first_el;
+            }
+            return true;
+        }
+    }
+}
+
+template <class T>
+void CircularArray<T>::sort()
+{
+    if (is_empty())
+        throw "Array is empty";
+    else
+    {
+        int var = front;
+        do
+        {
+            int pvt = next(var);
+            while (pvt != next(back))
+            {
+                if (*(this->array + var) > *(this->array + pvt))
+                {
+                    int tmp = *(this->array + var);
+                    *(this->array + var) = *(this->array + pvt);
+                    *(this->array + pvt) = tmp;
+                }
+                pvt = next(pvt);
+            }
+            var = next(var);
+        } while (next(var) != next(back));
+        if ((*this)[0] > (*this)[1])
+        {
+            int a = (*this)[0];
+            for (int i = 0; i < size() - 1; i++)
+            {
+                (*this)[i] = (*this)[i + 1];
+            }
+            (*this)[size() - 1] = a;
+        }
+    }
+}
+
+template <class T>
+void CircularArray<T>::reverse()
+{
+    int n = int(size() / 2);
+    for (int i = 0; i < n; i++)
+    {
+        swap((*this)[i], (*this)[size() - 1 - i]);
+    }
 }
 
 template <class T>
